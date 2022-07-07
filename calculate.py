b9839fe168444ee02b0c04ae23b75775ca84924f
almas <almas.giniatullin5@gmail.com>
2022-07-07 14:02:07 +0500

    some changes

--- calculate.py
+++ calculate.py
@@ -1,12 +1,17 @@
 import csv
+from math import sqrt
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 max_dist = {'len': 0, 'vec1': 0, 'vec2': 0}
 min_dist = {'len': 10000, 'vec1': 0, 'vec2': 0}
 
 
 def calculate_dists(vectors: list):
+    # max euclidean distance is sqrt(4m)
+    results_dict = {i / 10: 0 for i in range(0, round(sqrt(4*len(vectors[0])))*10)}
+    plt.figure()
     for i in range(len(vectors)):
         for j in range(len(vectors)):
             if i != j:
@@ -14,10 +19,13 @@ def calculate_dists(vectors: list):
                     max_dist['len'] = dist
                     max_dist['vec1'] = i + 1
                     max_dist['vec2'] = j + 1
-                elif dist:
+                elif dist < min_dist['len']:
                     min_dist['len'] = dist
                     min_dist['vec1'] = i + 1
                     min_dist['vec2'] = j + 1
+                results_dict[round(dist, 1)] += 1
+    plt.bar(list(results_dict.keys()), list(results_dict.values()), width=0.1)
+    plt.show()
 
 
 vectors = []
@@ -30,5 +38,5 @@ with open('vectors.csv', 'r') as file:
 
 calculate_dists(vectors)
 
-print(max_dist)
-print(min_dist)
+print(f'min_vec1: {min_dist["vec1"]}, min_vec2: {min_dist["vec2"]}, euclidean distance: {min_dist["len"]}')
+print(f'max_vec1: {max_dist["vec1"]}, max_vec2: {max_dist["vec2"]}, euclidean distance: {max_dist["len"]}')
--- create_csv.py
+++ create_csv.py
@@ -1,19 +1,11 @@
-import random
 import csv
-
 
 import numpy as np
 
 
 n_num, m_num = map(int, input().split())
 
-vectors = []
-for i in range(n_num):
-    vector = []
-    for j in range(m_num):
-        vector.append(random.uniform(-1, 1))
-    vector_np = np.array(vector)
-    vectors.append(vector_np)
+vectors = np.random.uniform(-1, +1, (n_num, m_num))
 
 with open('vectors.csv', 'w', newline='\n') as file:
     writer = csv.writer(file, delimiter=',')
